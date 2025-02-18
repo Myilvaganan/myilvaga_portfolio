e09502e06f7bbd02f237a9c668ef27b948fc1152
myilvaga <myilvaga@amazon.com>
2025-02-18 10:22:31 +0530

    Added Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
+
+const navLists = [
+    {
+        name: "Home",
+        link: "#home"
+    },
+    {
+        name: "About",
+        link: "#about"
+    },
+    {
+        name: "Skills",
+        link: "#skills"
+    },
+    {
+        name: "Services",
+        link: "#services"
+    },
+];
+
+function Header() {
+    const [isShining, setIsShining] = useState(false);
+
+    const handleClick = () => {
+        setIsShining(true);
+        setTimeout(() => setIsShining(false), 600); // Reset after animation
+    };
+
+    return (
+        <Navbar expand="lg" style={{padding: "10px" }}>
+            <Container>
+                <Navbar.Brand href="#" style={{ color: "aliceblue", fontWeight: "bold" }}>
+                    TECHIE <span style={{ color: "#00bf63" }}>MYIL</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="highlight bg-highlight"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-center w-100">
+                        {
+                            navLists.map((navList) => (
+                                <Nav.Link key={navList.link} href={navList.link} style={{ color: "aliceblue", marginRight: "15px" }}>{navList.name}</Nav.Link>
+                            ))
+                        }
+                    </Nav>
+                    <Button id="contact-button"     className={`contact-button ${isShining ? "shine" : ""}`}
+                            onClick={handleClick}>
+                        <strong>Contact</strong>
+                    </Button>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
