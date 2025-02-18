0619bd09c26e9e496b572d2362267edfd4bf2187
myilvaga <myilvaga@amazon.com>
2025-02-18 20:48:29 +0530

    Added name box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,15 @@ import Header from "../components/Header.tsx";
 export const Home = () => {
     return (
         <main id="home" className="layout-height">
-            <Header />
+            <div className="header-box">
+                <Header/>
+            </div>
+            <div className="name-box">
+                <p className="pr-text">Hello, <span className="highlight">I'm</span></p>
+                <p className="pr-text name">Myil<span className="highlight">vaganan</span></p>
+                <p className="pr-text">Web Developer | Software Engineer | Video Editor</p>
+            </div>
+
         </main>
     )
 }
